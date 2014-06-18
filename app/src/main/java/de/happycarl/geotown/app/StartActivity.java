4c8494361ff4a9bfcc8e6324f0ac3747ebb5a6c2
Belogron <ole.wehrmeyer@ewetel.net>
2014-06-18 18:11:22 +0200

    Added authorized api backend connection

--- app/src/main/java/de/happycarl/geotown/app/StartActivity.java
+++ app/src/main/java/de/happycarl/geotown/app/StartActivity.java
@@ -2,15 +2,21 @@ package de.happycarl.geotown.app;
 
 import android.annotation.TargetApi;
 import android.app.Activity;
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.TextView;
 
+import com.appspot.drive_log.geotown.Geotown;
+import com.appspot.drive_log.geotown.model.Route;
+import com.appspot.drive_log.geotown.model.RouteCollection;
 import com.google.api.client.googleapis.extensions.android.gms.auth.GoogleAccountCredential;
 import com.readystatesoftware.systembartint.SystemBarTintManager;
 
@@ -18,15 +24,27 @@ import butterknife.ButterKnife;
 import butterknife.InjectView;
 import butterknife.OnClick;
 
+import android.accounts.Account;
+import android.accounts.AccountManager;
 
+import com.google.android.gms.auth.GoogleAuthException;
+import com.google.android.gms.auth.GoogleAuthUtil;
+import com.google.android.gms.common.AccountPicker;
+import com.google.api.client.googleapis.extensions.android.gms.auth.GoogleAccountCredential;
+
+import java.io.IOException;
 
 
 public class StartActivity extends Activity {
 
+    static final int REQUEST_ACCOUNT_PICKER = 2;
+
+
     SharedPreferences settings;
     GoogleAccountCredential credential;
     String accountName;
-    final String PREF_ACCOUNT_NAME="account_name";
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,17 +56,36 @@ public class StartActivity extends Activity {
     }
 
     @OnClick(R.id.start_button)
-    protected void buttonClicked() {
+    protected void loginGoogle() {
+        if(!AppConstants.checkGooglePlayServicesAvailable(this))
+            return;
         settings = getSharedPreferences(
-                "GeoTown", 0);
+                AppConstants.PREF_NAME, 0);
         credential = GoogleAccountCredential.usingAudience(this,
-                "1005962513631-3tq5l9qibm5aolt87sp32a2ib9ga6in0.apps.googleusercontent.com");
+                AppConstants.CLIENT_ID);;
 
-        setSelectedAccountName(settings.getString(PREF_ACCOUNT_NAME,null));
+        setSelectedAccountName(settings.getString(AppConstants.PREF_ACCOUNT_NAME,null));
+        AppConstants.geotownInstance = AppConstants.getApiServiceHandle(credential);
 
+        if(credential.getSelectedAccountName() != null) {
+            //Already signed in
+            Log.d("Login","Successfully logged in");
+
+            //I somewhat should be redirecting people here...
+        } else {
+            Log.d("Login","Showing account picker");
+            chooseAccount();
+            loginGoogle();
+        }
 
 
     }
+
+    private void chooseAccount() {
+        startActivityForResult(credential.newChooseAccountIntent(),
+                REQUEST_ACCOUNT_PICKER);
+    }
+
 
     private void initSystemBarTint() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
@@ -97,11 +134,36 @@ public class StartActivity extends Activity {
     // setSelectedAccountName definition
     private void setSelectedAccountName(String accountName) {
         SharedPreferences.Editor editor = settings.edit();
-        editor.putString(PREF_ACCOUNT_NAME, accountName);
+        editor.putString(AppConstants.PREF_ACCOUNT_NAME, accountName);
         editor.commit();
         credential.setSelectedAccountName(accountName);
         this.accountName = accountName;
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode,
+                                    Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case REQUEST_ACCOUNT_PICKER:
+                if (data != null && data.getExtras() != null) {
+                    String accountName =
+                            data.getExtras().getString(
+                                    AccountManager.KEY_ACCOUNT_NAME);
+                    if (accountName != null) {
+                        setSelectedAccountName(accountName);
+                        SharedPreferences.Editor editor = settings.edit();
+                        editor.putString(AppConstants.PREF_ACCOUNT_NAME, accountName);
+                        editor.commit();
+                        // User is authorized.
+                    }
+                }
+                break;
+        }
+    }
+
+
+
 
 
 }
