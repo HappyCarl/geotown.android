f592be232690919aacebe67de03b638866c04f89
Belogron <ole.wehrmeyer@ewetel.net>
2014-06-19 21:07:53 +0200

    Added image preview to route

---
+++ app/src/main/java/de/happycarl/geotown/app/gui/CardTarget.java
@@ -0,0 +1,44 @@
+package de.happycarl.geotown.app.gui;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
+import android.util.Log;
+
+import com.afollestad.cardsui.Card;
+import com.afollestad.cardsui.CardAdapter;
+import com.squareup.picasso.Picasso;
+import com.squareup.picasso.Target;
+
+/**
+ * Created by ole on 19.06.14.
+ */
+public class CardTarget implements Target {
+
+    Card c;
+    Context con;
+    CardAdapter adapter;
+
+    public CardTarget(Card card, Context context, CardAdapter adapter) {
+        c = card;
+        con = context;
+        this.adapter = adapter;
+    }
+
+    @Override
+    public void onBitmapLoaded(Bitmap bitmap, Picasso.LoadedFrom loadedFrom) {
+        Log.d("Picasso", "Loaded Bitmap");
+        c.setThumbnail(con, bitmap);
+        adapter.update(c, true);
+    }
+
+    @Override
+    public void onBitmapFailed(Drawable drawable) {
+
+    }
+
+    @Override
+    public void onPrepareLoad(Drawable drawable) {
+
+    }
+}
--- app/src/main/java/de/happycarl/geotown/app/gui/OverviewActivity.java
+++ app/src/main/java/de/happycarl/geotown/app/gui/OverviewActivity.java
@@ -1,8 +1,13 @@
 package de.happycarl.geotown.app.gui;
 
 import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
 import android.text.Html;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
@@ -14,9 +19,14 @@ import com.afollestad.cardsui.CardHeader;
 import com.afollestad.cardsui.CardListView;
 import com.appspot.drive_log.geotown.model.Route;
 import com.squareup.otto.Subscribe;
+import com.squareup.picasso.Picasso;
+import com.squareup.picasso.Target;
+
+import java.io.IOException;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
+import butterknife.OnClick;
 import de.happycarl.geotown.app.AppConstants;
 import de.happycarl.geotown.app.GeotownApplication;
 import de.happycarl.geotown.app.R;
@@ -31,6 +41,8 @@ public class OverviewActivity extends Activity {
     @InjectView(R.id.overview_user)
     TextView userText;
 
+
+
     CardAdapter adapter;
 
     @Override
@@ -44,11 +56,9 @@ public class OverviewActivity extends Activity {
         String userEmail = GeotownApplication.getPreferences().getString(AppConstants.PREF_ACCOUNT_NAME, "");
         userText.setText(Html.fromHtml("<i>" + userEmail + "</i>"));
 
+
         adapter = new CardAdapter(this, android.R.color.holo_red_light);
         cardListView.setAdapter(adapter);
-
-        CardHeader header = new CardHeader(getResources().getString(R.string.my_routes));
-        adapter.add(header);
 
         AllMyRoutesRequest routesRequest = new AllMyRoutesRequest();
         routesRequest.execute((Void) null);
@@ -63,14 +73,32 @@ public class OverviewActivity extends Activity {
         return true;
     }
 
+
+    protected void exitButton() {
+        finish();
+    }
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
+        switch (id) {
+            //case R.id.action_settings:
+            //    return true;
+            case R.id.action_close:
+                finish();
+                return true;
+            case R.id.action_about:
+
+                break;
+            case R.id.action_refresh:
+                adapter.clear();
+                AllMyRoutesRequest routesRequest = new AllMyRoutesRequest();
+                routesRequest.execute((Void) null);
+                break;
+
         }
         return super.onOptionsItemSelected(item);
     }
@@ -83,12 +111,48 @@ public class OverviewActivity extends Activity {
             adapter.add(empty);
             return;
         }
+
+
+
+        CardHeader header = new CardHeader(getResources().getString(R.string.my_routes));
+        adapter.add(header);
         for (Route r : event.routes) {
-            Card c = new Card(r.getName(), r.getLatitude() + "/" + r.getLongitude());
+
+
+            Card c = new Card(r.getName(), getLocationName(r.getLatitude(), r.getLongitude()));
             //TODO:Add image via Picasso
+            CardTarget target = new CardTarget(c,this,adapter);
+            Picasso.with(this).load("https://maps.google.com/maps/api/staticmap?center=" + r.getLatitude() + ","+r.getLongitude()+"&size=128x128&zoom=8").into(target);
             adapter.add(c);
+
         }
     }
+
+    private String getLocationName(double latitude, double longitude) {
+        String result = getResources().getString(R.string.unknown_place);
+
+        Geocoder gc = new Geocoder(this);
+
+        try {
+            Address address = gc.getFromLocation(latitude, longitude, 1).get(0);
+
+            String town = getResources().getString(R.string.unknown_town);
+            if(address.getLocality()!=null) {
+                town = address.getLocality();
+            }
+            String country = getResources().getString(R.string.unknown_country);
+            if(address.getCountryName() != null) {
+                country = address.getCountryName();
+            }
+            result = town + ", " + country;
+        } catch (IOException | IndexOutOfBoundsException e) {
+            e.printStackTrace();
+        }
+
+        return result;
+    }
+
+
 
 
 }
