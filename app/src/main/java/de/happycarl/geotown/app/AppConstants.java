4a49ad886a2ff07b63da854f6387849ebf4cf091
Jan-Henrik <jh.bruhn@me.com>
2014-06-20 23:21:15 +0200

    Improved AppConstants.

--- app/src/main/java/de/happycarl/geotown/app/AppConstants.java
+++ app/src/main/java/de/happycarl/geotown/app/AppConstants.java
@@ -1,23 +1,5 @@
 package de.happycarl.geotown.app;
 
-import android.accounts.Account;
-import android.accounts.AccountManager;
-import android.app.Activity;
-import android.app.Dialog;
-import android.content.Context;
-
-import com.appspot.drive_log.geotown.Geotown;
-import com.google.android.gms.auth.GoogleAuthUtil;
-import com.google.android.gms.common.GooglePlayServicesUtil;
-import com.google.api.client.extensions.android.http.AndroidHttp;
-import com.google.api.client.extensions.android.json.AndroidJsonFactory;
-import com.google.api.client.googleapis.extensions.android.gms.auth.GoogleAccountCredential;
-import com.google.api.client.http.HttpTransport;
-import com.google.api.client.json.JsonFactory;
-
-import javax.annotation.Nullable;
-
-
 /**
  * Created by ole on 18.06.14.
  */
@@ -29,7 +11,5 @@ public class AppConstants {
     //shared prefs stuff
     public static final String PREF_NAME = "GeoTown";
     public static final String PREF_ACCOUNT_NAME = "account_name";
-
-
 
 }
