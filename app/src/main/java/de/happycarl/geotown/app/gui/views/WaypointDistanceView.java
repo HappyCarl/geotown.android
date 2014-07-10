0ce2a3f175b5448c31ea606374e1d82c69a5a48b
Ole Wehrmeyer <ole.wehrmeyer@ewetel.net>
2014-07-10 15:22:41 +0200

    Fixed view, not working, font drawing needs some visual optimizations

--- app/src/main/java/de/happycarl/geotown/app/gui/views/WaypointDistanceView.java
+++ app/src/main/java/de/happycarl/geotown/app/gui/views/WaypointDistanceView.java
@@ -22,8 +22,14 @@ public class WaypointDistanceView extends View {
     private Paint circlePaint;
     private Paint circleHighlightPaint;
 
+    private Paint centerPaint;
+    private Paint centerHighlightPaint;
+
+    private Paint boxPaint;
+    private Paint textPaint;
+
     private float circleMaxDiameter = 0;
-    private float centerX, centerY;
+    private float centerX, centerY, textOffX;
 
     public WaypointDistanceView(Context context) {
         super(context);
@@ -45,13 +51,32 @@ public class WaypointDistanceView extends View {
 
         circlePaint = new Paint(0);
         circlePaint.setStyle(Paint.Style.STROKE);
-        circlePaint.setColor(0xff303030);
+        circlePaint.setColor(getResources().getColor(android.R.color.darker_gray));
         circlePaint.setStrokeWidth(10f);
 
         circleHighlightPaint = new Paint(0);
         circleHighlightPaint.setStyle(Paint.Style.STROKE);
-        circleHighlightPaint.setColor(0xffff0000);
+        circleHighlightPaint.setColor(getResources().getColor(android.R.color.holo_red_light));
         circleHighlightPaint.setStrokeWidth(10f);
+
+        centerPaint = new Paint(0);
+        centerPaint.setStyle(Paint.Style.FILL_AND_STROKE);
+        centerPaint.setColor(getResources().getColor(android.R.color.darker_gray));
+        centerPaint.setStrokeWidth(10f);
+
+        centerHighlightPaint = new Paint(0);
+        centerHighlightPaint.setStyle(Paint.Style.FILL_AND_STROKE);
+        centerHighlightPaint.setColor(getResources().getColor(android.R.color.holo_red_light));
+        centerHighlightPaint.setStrokeWidth(10f);
+
+        textPaint = new Paint(0);
+        textPaint.setColor(getResources().getColor(android.R.color.primary_text_light));
+        textPaint.setTextAlign(Paint.Align.CENTER);
+        textPaint.setTextSize(80);
+
+        boxPaint = new Paint(0);
+        boxPaint.setColor(getResources().getColor(android.R.color.background_light));
+        boxPaint.setStyle(Paint.Style.FILL);
     }
 
     public int getDistance() {
@@ -60,6 +85,8 @@ public class WaypointDistanceView extends View {
 
     public void setDistance(int dist) {
         distance = dist;
+        String distStr = dist + " m";
+        textOffX = textPaint.getTextSize() * (distStr.length() / 2);
         invalidate();
         requestLayout();
     }
@@ -68,28 +95,34 @@ public class WaypointDistanceView extends View {
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
-        for (int i = 200; i <= 600; i += 200) {
-            Log.d("onDraw", "" + i + " : " + distance);
+        canvas.drawCircle(centerX, centerY, circleMaxDiameter / 35, (distance < 200)? centerHighlightPaint : centerPaint);
+
+        for (int i = 200; i <= 800; i += 200) {
+            Log.d("onDraw", "" + i + " : " + distance + " : " + (circleMaxDiameter / 2) * ((float)i / 1000));
             Paint paint = circlePaint;
-            if(distance > i)
+            if(distance >= i && distance < i + 200)
                 paint = circleHighlightPaint;
-            canvas.drawCircle(centerX, centerY, (circleMaxDiameter / 2) * (i / 600), paint);
+            canvas.drawCircle(centerX, centerY, (circleMaxDiameter / 2) * ((float)i / 1000), paint);
         }
 
+        canvas.drawCircle(centerX, centerY, circleMaxDiameter / 2, (distance >= 1000)? circleHighlightPaint : circlePaint);
+
+
+
+        canvas.drawText(distance + " m", centerX, (float) (centerY * 1.3), textPaint );
 
     }
 
     @Override
     protected void onSizeChanged(int w, int h, int oldw, int oldh) {
-        Log.d("OnSizeChanged", "size: " + w + ":" + h);
-        float realWidth = w - (getPaddingLeft() + getPaddingRight());
-        float realHeight = h - (getPaddingBottom() + getPaddingTop());
+        Log.d("OnSizeChanged", "size: " + w + ":" + h + "; pos: " + getX() + ":" + getY());
 
-        circleMaxDiameter = Math.min(realWidth,realHeight);
+
+        circleMaxDiameter = Math.min(w,h);
         circleMaxDiameter -= (circleMaxDiameter / 10);
 
-        centerX = getX() + realWidth / 2;
-        centerY = getY() + realHeight / 2;
+        centerX = w / 2;
+        centerY = h / 2;
         Log.d("OnSizeChanged", "circle " + centerX + ":" + centerY + " : " + circleMaxDiameter);
     }
 
