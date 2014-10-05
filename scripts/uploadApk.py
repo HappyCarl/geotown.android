1e27e6a2bf66c9fa216942e039c0d19fad5d0438
Jan-Henrik Bruhn <jh.bruhn@me.com>
2014-10-05 12:22:20 +0200

    Implemented automatic APK upload to google play

---
+++ scripts/uploadApk.py
@@ -0,0 +1,85 @@
+"""Lists all the apks for a given app."""
+
+import argparse
+
+from apiclient.discovery import build
+import httplib2
+from oauth2client import client
+
+
+SERVICE_ACCOUNT_EMAIL = (
+    '626683764494-k97n7647fh4fctik7li5l467i3r8pnhm@developer.gserviceaccount.com')
+
+# Declare command-line flags.
+argparser = argparse.ArgumentParser(add_help=False)
+argparser.add_argument('package_name',
+                       help='The package name. Example: com.android.sample')
+argparser.add_argument('track',
+                       default='alpha',
+                       help='The Track. production, beta, alpha')
+argparser.add_argument('apk_file',
+                       nargs='?',
+                       default='test.apk',
+                       help='The path to the APK file to upload.')
+
+
+def main():
+  # Load the key in PKCS 12 format that you downloaded from the Google APIs
+  # Console when you created your Service account.
+  f = file('key.p12', 'rb')
+  key = f.read()
+  f.close()
+
+  # Create an httplib2.Http object to handle our HTTP requests and authorize it
+  # with the Credentials. Note that the first parameter, service_account_name,
+  # is the Email address created for the Service account. It must be the email
+  # address associated with the key that was created.
+  credentials = client.SignedJwtAssertionCredentials(
+      SERVICE_ACCOUNT_EMAIL,
+      key,
+      scope='https://www.googleapis.com/auth/androidpublisher',
+      private_key_password='notasecret')
+  http = httplib2.Http()
+  http = credentials.authorize(http)
+
+  service = build('androidpublisher', 'v2', http=http)
+
+  # Process flags and read their values.
+  flags = argparser.parse_args()
+
+  package_name = flags.package_name
+  track = flags.track
+  apk_file = flags.apk_file
+
+  try:
+
+    edit_request = service.edits().insert(body={}, packageName=package_name)
+    result = edit_request.execute()
+    edit_id = result['id']
+
+    apk_response = service.edits().apks().upload(
+        editId=edit_id,
+        packageName=package_name,
+        media_body=apk_file).execute()
+
+    print 'Version code %d has been uploaded' % apk_response['versionCode']
+
+    track_response = service.edits().tracks().update(
+        editId=edit_id,
+        track=track,
+        packageName=package_name,
+        body={u'versionCodes': [apk_response['versionCode']]}).execute()
+
+    print 'Track %s is set for version code(s) %s' % (
+        track_response['track'], str(track_response['versionCodes']))
+
+    commit_request = service.edits().commit(
+        editId=edit_id, packageName=package_name).execute()
+    print 'Edit "%s" has been committed' % (commit_request['id'])
+
+  except client.AccessTokenRefreshError:
+    print ('The credentials have been revoked or expired, please re-run the '
+           'application to re-authorize')
+
+if __name__ == '__main__':
+  main()
