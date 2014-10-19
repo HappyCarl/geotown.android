73feb8a21176ea7808a6e7d82bf595bc6204b829
Jan-Henrik Bruhn <jh.bruhn@me.com>
2014-10-19 17:51:08 +0200

    Moved WaypointDistanceView to library project (to lateron use it on android wear)

--- common/src/main/java/de/happycarl/geotown/app/common/views/WaypointDistanceView.java
+++ common/src/main/java/de/happycarl/geotown/app/common/views/WaypointDistanceView.java
@@ -1,10 +1,9 @@
-package de.happycarl.geotown.app.gui.views;
+package de.happycarl.geotown.app.common.views;
 
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.Path;
-import android.graphics.Point;
 import android.hardware.GeomagneticField;
 import android.util.AttributeSet;
 import android.util.Log;
